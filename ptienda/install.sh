8686693f9ca19f5c001e8728a6663d59245ca9f0
Jorge <jorgeperez793@gmail.com>
2023-02-04 22:31:48 +0100

    first commit

---
+++ ptienda/install.sh
@@ -0,0 +1,10 @@
+echo "Tareas (por etapas) del pipleline Jenkinsfile"
+
+echo "Instalando la tienda App Pc Compoentes"
+echo "Descargar repositorio de appTienda...."
+echo "Actualizar versiones de los dockerfiler en docker Hub..."
+echo "Realiza el build del proyecto"
+echo "Fases de testeo"
+echo "Martar los contenedores en ejecucion de la version anterior"
+echo "Levantar los nuevos contenedores actualizados"
+
