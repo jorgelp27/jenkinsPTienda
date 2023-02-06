2588d3e9ef7fd1d0b24263d7f83d3d6358a202c8
Jorge <jorgeperez793@gmail.com>
2023-02-06 14:00:35 +0100

    prueba

--- ptienda/install.sh
+++ ptienda/install.sh
@@ -1,10 +1,14 @@
-echo "Tareas (por etapas) del pipleline Jenkinsfile"
+# echo "Tareas (por etapas) del pipleline Jenkinsfile"
 
-echo "Instalando la tienda App Pc Compoentes"
-echo "Descargar repositorio de appTienda...."
-echo "Actualizar versiones de los dockerfiler en docker Hub..."
-echo "Realiza el build del proyecto"
-echo "Fases de testeo"
-echo "Martar los contenedores en ejecucion de la version anterior"
-echo "Levantar los nuevos contenedores actualizados"
+# echo "Instalando la tienda App Pc Compoentes"
+# echo "Descargar repositorio de appTienda...."
+# echo "Actualizar versiones de los dockerfiler en docker Hub..."
+# echo "Realiza el build del proyecto"
+# echo "Fases de testeo"
+# echo "Martar los contenedores en ejecucion de la version anterior"
+# echo "Levantar los nuevos contenedores actualizados"
 
+echo "Desplegando la app de ptienda"
+
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up -d
