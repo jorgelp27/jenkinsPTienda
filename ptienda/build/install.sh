76d87d93bee1a478b89e58a8d3b65263076d8316
Jorge <jorgeperez793@gmail.com>
2023-02-14 20:13:21 +0100

    despliegue4

--- ptienda/build/install.sh
+++ ptienda/build/install.sh
@@ -10,8 +10,8 @@
 
 echo "Desplegando la app de ptienda"
 
-docker-compose -f ptienda/build/docker-compose.yml build
-docker-compose -f ptienda/build/docker-compose.yml up -d
+docker-compose -f ptienda/build/Dokerfiles/ubuntu/nest/docker-compose.yml build
+docker-compose -f ptienda/build/Dokerfiles/ubuntu/nest/docker-compose.yml up -d
 
 # docker-compose -f ptienda/build/docker-compose.yml build --no-cache
 # docker-compose -f ptienda/build/docker-compose.yml down
