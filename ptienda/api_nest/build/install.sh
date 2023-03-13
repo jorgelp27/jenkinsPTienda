311234dbfcf36420b2a8686be38d82da2e549b50
Jorge <jorgeperez793@gmail.com>
2023-03-13 19:43:43 +0100

    pruba3

---
+++ ptienda/api_nest/build/install.sh
@@ -0,0 +1,16 @@
+
+echo "Desplegando la app de ptienda"
+
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml build
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml up -d
+
+# docker-compose -f ptienda/build/docker-compose.yml build --no-cache
+# docker-compose -f ptienda/build/docker-compose.yml down
+# docker-compose -f ptienda/build/docker-compose.yml up --remove-orphans
+
+
+
+
+
+
+
