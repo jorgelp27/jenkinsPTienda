98688fd98805b690cda84cc04b14d4b0aaf14afb
Jorge <jorgeperez793@gmail.com>
2023-03-13 13:30:54 +0100

    prueba

--- ptienda/build/install.sh
+++ ptienda/build/install.sh
@@ -1,21 +1,14 @@
-# echo "Tareas (por etapas) del pipleline Jenkinsfile"
-
-# echo "Instalando la tienda App Pc Compoentes"
-# echo "Descargar repositorio de appTienda...."
-# echo "Actualizar versiones de los dockerfiler en docker Hub..."
-# echo "Realiza el build del proyecto"
-# echo "Fases de testeo"
-# echo "Martar los contenedores en ejecucion de la version anterior"
-# echo "Levantar los nuevos contenedores actualizados"
 
 echo "Desplegando la app de ptienda"
 
-docker-compose -f ptienda/build/docker-compose.yml build
-docker-compose -f ptienda/build/docker-compose.yml up -d
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml build
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml up -d
 
 # docker-compose -f ptienda/build/docker-compose.yml build --no-cache
 # docker-compose -f ptienda/build/docker-compose.yml down
 # docker-compose -f ptienda/build/docker-compose.yml up --remove-orphans
+
+
 
 
 
