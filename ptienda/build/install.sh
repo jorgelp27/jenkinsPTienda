c9c900774a531208f41865a88ea3b55ae8e99242
Jorge <jorgeperez793@gmail.com>
2023-03-16 19:29:18 +0100

    prueba3

--- ptienda/build/install.sh
+++ ptienda/build/install.sh
@@ -6,7 +6,7 @@ echo "Desplegando la app de ptienda"
 
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/api_nest/docker-compose.yml build --no-cache
 
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/api_nest/docker-compose.yml up
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/api_nest/docker-compose.yml up -d
 
 # docker-compose -f ptienda/build/docker-compose.yml build --no-cache
 # docker-compose -f ptienda/build/docker-compose.yml down
